a3c77fc25a03b8817865b57eab719300f8788a0a
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2025-03-09 15:36:01 +0700

    added MoveOnlyScopedPointer

--- task5/scopedpointer.cpp
+++ task5/scopedpointer.cpp
@@ -2,11 +2,11 @@
 
 
 template <typename T>
-class ScopedPointer {
+class DeepCopyScopedPointerScopedPointer {
 	T* pointer;
 
 public:
-	explicit ScopedPointer(T* raw) : pointer(raw) {}
+	explicit DeepCopyScopedPointerScopedPointer(T* raw) : pointer(raw) {}
 
 	DeepCopyScopedPointer(const DeepCopyScopedPointer& other)
 		: pointer(other.pointer ? new T(*other.pointer) : nullptr) {
@@ -22,13 +22,12 @@ public:
 		return *this;
 	}
 
-	DeepCopyScopedPointer(DeepCopyScopedPointer&& other) noexcept
+	DeepCopyScopedPointer(DeepCopyScopedPointer&& other)
 		: pointer(other.pointer) {
 		other.pointer = nullptr;
 	}
 
-	// Move assignment
-	DeepCopyScopedPointer& operator=(DeepCopyScopedPointer&& other) noexcept {
+	DeepCopyScopedPointer& operator=(DeepCopyScopedPointer&& other) {
 		if (this == &other)
 			return *this;
 
@@ -42,5 +41,39 @@ public:
 	T& get() { return *pointer; }
 	const T& get() const { return *pointer; }
 
-	~ScopedPointer() { delete pointer; }
+	~DeepCopyScopedPointerScopedPointer() { delete pointer; }
+};
+
+
+template <typename T>
+class MoveOnlyScopedPointer {
+	T* pointer;
+
+public:
+	explicit MoveOnlyScopedPointer(T* raw) : pointer(raw) {}
+
+	MoveOnlyScopedPointer(const MoveOnlyScopedPointer&) = delete;
+	MoveOnlyScopedPointer& operator=(const MoveOnlyScopedPointer&) = delete;
+
+	MoveOnlyScopedPointer(MoveOnlyScopedPointer&& other)
+		: pointer(other.pointer) {
+		other.pointer = nullptr;
+	}
+
+	MoveOnlyScopedPointer& operator=(MoveOnlyScopedPointer&& other) {
+		if (this == &other)
+			return *this;
+
+		delete pointer;
+		pointer = other.pointer;
+		other.pointer = nullptr;
+
+		return *this;
+	}
+
+
+	T& get() { return *pointer; }
+	const t& get() const { return *pointer; }
+
+	~MoveOnlyScopedPointer() { delete pointer; }
 };
