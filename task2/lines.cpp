6226b1aad0776d388c5931382cc06c0cbc353c5c
Alexey Shishkin <AlexisV.Shishkin@gmail.com>
2025-03-09 14:22:33 +0300

    some fixes

--- task2/lines.cpp
+++ task2/lines.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 #include <optional>
+#include <cmath>
+
 
 struct Point {
     double x, y;
-    
+
     Point(double x_, double y_) : x(x_), y(y_) {}
 };
+
+
+bool equals(double a, double b, double tolerance = 1e-9) {
+    return std::fabs(a - b) < tolerance;
+}
+
 
 class Line {
 private:
     double a, b, c; // ax + by + c = 0
 
-public:
-    Line(double a_, double b_, double c_) : a(a_), b(b_), c(c_) {}
+    bool isValid() const {
+        return !(std::isnan(a) || std::isnan(b) || std::isnan(c));
+    }
 
+public:
     Line(const Point& p1, const Point& p2) {
-        a = p2.y - p1.y;
-        b = p1.x - p2.x;
-        c = -(a * p1.x + b * p1.y);
+        if (equals(p1.x, p2.x) && equals(p1.y, p2.y)) {
+            a = b = c = std::nan("1");
+        }
+        else {
+            a = p2.y - p1.y;
+            b = p1.x - p2.x;
+            c = -(a * p1.x + b * p1.y);
+        }
     }
 
     std::optional<Point> intersection(const Line& other) const {
-        double det = a * other.b - other.a * b;
-        if (det == 0) {
+        if (!isValid()) {
             return std::nullopt;
         }
+
+        double det = a * other.b - other.a * b;
+        if (equals(det, 0)) {
+            return std::nullopt;
+        }
+
         double x = (other.b * (-c) - b * (-other.c)) / det;
         double y = (a * (-other.c) - other.a * (-c)) / det;
         return Point(x, y);
@@ -35,15 +55,23 @@ public:
     }
 
     void print() const {
-        std::cout << a << "x + " << b << "y + " << c << " = 0" << std::endl;
+        if (isValid()) {
+            std::cout << a << "x + " << b << "y + " << c << " = 0" << std::endl;
+        }
+        else {
+            std::cout << "Invalid line!" << std::endl;
+        }
+    }
+
+    bool operator==(const Line& other) const {
+        return equals(a, other.a) && equals(b, other.b) && equals(c, other.c);
     }
 };
-
 
 int main() {
     Point p1(0, 0), p2(4, 4);
     Line line1(p1, p2);
-    
+
     Point p3(0, 4), p4(4, 0);
     Line line2(p3, p4);
 
@@ -55,13 +83,18 @@ int main() {
     auto inter = line1.intersection(line2);
     if (inter) {
         std::cout << "Intersection: (" << inter->x << ", " << inter->y << ")\n";
-    } else {
+    }
+    else {
         std::cout << "No intersection (parallel lines).\n";
     }
 
     Line perp = line1.perpendicularAt(Point(2, 2));
     std::cout << "Perpendicular to Line 1 through (2,2): ";
     perp.print();
+
+    Line invalidLine(Point(1, 1), Point(1, 1));
+    std::cout << "Invalid line: ";
+    invalidLine.print();
 
     return 0;
 }
